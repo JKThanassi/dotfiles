7be2e5977635f81655502d6d886e795de298ff08
Joseph Kaming-Thanassi <josephkamingthanassi@gmail.com>
2026-02-18 14:01:17 -0800

    use project root tsconfig but local eslint

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,8 +5,12 @@ vim.g.maplocalleader = "\\"
 
 vim.opt.cmdheight = 2         -- LazyVim default is 1
 vim.opt.signcolumn = "number" -- Merge sign column with number column (LazyVim default is "yes")
+vim.opt.numberwidth = 1       -- Minimum width; expands only as needed for digit count
 vim.opt.timeout = false       -- Don't timeout on mappings
 vim.opt.ttimeout = true       -- Do timeout on keycodes
 vim.opt.ttimeoutlen = 200     -- Wait up to 200ms for keycode sequence
 
 vim.g.tex_flavor = "latex"    -- Treat .tex files as LaTeX
+
+-- Check nearest package.json / tsconfig.json before asking LSP — avoids monorepo root bleed
+vim.g.root_spec = { { "package.json", "tsconfig.json", ".git", "lua" }, "lsp", "cwd" }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,4 +1,32 @@
 return {
+  -- Override vtsls root_dir using nvim 0.11's async API so it anchors at the nearest
+  -- package.json / tsconfig.json, preventing tsconfig "extends" chains from climbing
+  -- up to the monorepo root.
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        vtsls = {
+          -- root_dir = function(bufnr, on_dir)
+          --   local root = vim.fs.root(bufnr, { "package.json", "tsconfig.json", "jsconfig.json" })
+          --   if root then
+          --     on_dir(root)
+          --   end
+          -- end,
+          root_markers = { "package.json", "tsconfig.json", "jsconfig.json" },
+        },
+        eslint = {
+          root_dir = function(bufnr, on_dir)
+            local root = vim.fs.root(bufnr, { "package.json", ".eslintrc.js", ".eslintrc.cjs", "eslint.config.mjs", "eslint.config.js" })
+            if root then
+              on_dir(root)
+            end
+          end,
+        },
+      },
+    },
+  },
+
   -- Configure prettier formatting for web file types
   {
     "stevearc/conform.nvim",
